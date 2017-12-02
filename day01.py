ad08d9badf288c5bd96647c7883a5e9be6ef37ae
Todd Griffin <nemagnusfrater@gmail.com>
2017-12-02 15:11:13 -0500

    Finish day 1.

--- day01.py
+++ day01.py
@@ -1,5 +1,47 @@
 # read in all the data
 with open('one') as f:
-	data = f.read()
+	data = f.readline()
 
-print(data)
+#
+#	Part 1
+#
+
+# sum of all doubles
+sum = 0
+
+# cycle through pairs
+i = 0
+while i < len(data) - 1:
+	# get the next index (wrap on end)
+	j = 0 if i == len(data) - 2 else i + 1
+
+	# if equal, sum
+	if data[i] == data[j]:
+		sum += int(data[i])
+	
+	i += 1
+
+# print result
+print(sum)
+
+#
+#	Part 2
+#
+
+sum = 0
+
+# cycle through pairs
+i = 0
+while i < len(data) - 1:
+	# get the next index
+	half = int((len(data) - 1) / 2)
+	j = i + half if i + half < len(data) - 1 else i - half
+
+	# if equal, sum
+	if data[i] == data[j]:
+		sum += int(data[i])
+
+	i += 1
+
+# print result
+print(sum)
