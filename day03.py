bb62294b199b1d43ed7f64fba42e1c5cdc614bf2
Todd Griffin <nemagnusfrater@gmail.com>
2017-12-04 03:46:52 -0500

    Finish day 3.

---
+++ day03.py
@@ -0,0 +1,153 @@
+# given input
+input = 289326
+
+#
+#	Part 1
+#
+
+class Onion (object):
+	layer = 0
+	smallestIndex = 1
+
+	# returns the number of indices within the given layer
+	def getLayerSize (self):
+		return self.layer * 8 if self.layer > 0 else 1
+
+	# returns the largest index found within the given layer
+	def getLargestIndex (self):
+		return self.getLayerSize() + self.smallestIndex - 1 if self.getLayerSize() > 1 else self.smallestIndex
+
+	# returns the inner side length of the onion (discluding corners)
+	def getSideLength (self):
+		return int((self.getLayerSize() - 4) / 4)
+
+	# moves to the next layer
+	def nextLayer (self):
+		self.smallestIndex = self.getLargestIndex() + 1
+		self.layer += 1
+
+	def toString (self):
+		print("layer", self.layer, "smallestIndex", self.smallestIndex, "largestIndex", self.getLargestIndex(), "size", self.getLayerSize())
+
+# returns the Manhattan Distance (taxi geometry) of the given index within the Onion
+def getManhattanDistance (index, onion):
+	x = onion.layer
+	y = (onion.layer - 1) * -1 if onion.smallestIndex > 2 else 0
+	sideLength = onion.getSideLength()
+
+	currentIndex = onion.smallestIndex
+
+	# check right-wall indices
+	for i in range(sideLength):
+		if index == currentIndex:
+			return abs(x) + abs(y)
+		else:
+			currentIndex += 1
+			y += 1
+
+	# check top-wall indices
+	for i in range(sideLength + 1):
+		if index == currentIndex:
+			return abs(x) + abs(y)
+		else:
+			currentIndex += 1
+			x -= 1
+	
+	# check left-wall indices
+	for i in range(sideLength + 1):
+		if index == currentIndex:
+			return abs(x) + abs(y)
+		else:
+			currentIndex += 1
+			y -= 1
+	
+	# check bottom-wall indices
+	for i in range(sideLength + 1):
+		if index == currentIndex:
+			return abs(x) + abs(y)
+		else:
+			currentIndex += 1
+			x += 1
+
+def part1 (index):
+	onion = Onion()
+
+	while index > onion.getLargestIndex():
+		onion.nextLayer()
+
+	return getManhattanDistance(index, onion)
+
+distance = part1(input)
+print(distance)
+
+#
+#	Part 2
+#
+
+# returns the memory's key given a Cartesian coordinate
+def getKey (x, y):
+	return str(x) + "|" + str(y)
+
+# writes the sum of all surrounding memory units (that have been written already) into the memory at the given Cartesian coordinate
+# returns the sum
+def write (memory, x, y):
+	sum = 0
+
+	sum += memory.get(getKey(x, y+1), 0)	# above
+	sum += memory.get(getKey(x, y-1), 0)	# below
+	sum += memory.get(getKey(x-1, y), 0)	# left
+	sum += memory.get(getKey(x+1, y), 0)	# right
+
+	sum += memory.get(getKey(x-1, y+1), 0)	# top-left
+	sum += memory.get(getKey(x+1, y+1), 0)	# top-right
+	sum += memory.get(getKey(x-1, y-1), 0)	# bottom-left
+	sum += memory.get(getKey(x+1, y-1), 0)	# bottom-right
+
+	memory[getKey(x, y)] = sum
+
+	return sum
+
+def part2 (input):
+	# prepare the Onion
+	onion = Onion()
+	x = y = 0
+	currentIndex = 1
+	memory = {}
+
+	# handle the first index
+	memory[getKey(x,y)] = 1
+	x += 1
+	onion.nextLayer()
+
+	# handle each layer of the Onion
+	while True:
+		sideLength = onion.getSideLength()
+
+		# write right-wall values
+		for i in range(sideLength):
+			if write(memory, x, y) > input:
+				return memory[getKey(x,y)]
+			y += 1
+
+		# write top-wall values
+		for i in range(sideLength + 1):
+			if write(memory, x, y) > input:
+				return memory[getKey(x,y)]
+			x -= 1
+
+		# write left-wall values
+		for i in range(sideLength + 1):
+			if write(memory, x, y) > input:
+				return memory[getKey(x,y)]
+			y -= 1
+
+		# write bottom-wall values
+		for i in range(sideLength + 2):
+			if write(memory, x, y) > input:
+				return memory[getKey(x,y)]
+			x += 1
+
+		onion.nextLayer()
+
+firstLargerValue = part2(input)
+print(firstLargerValue)
